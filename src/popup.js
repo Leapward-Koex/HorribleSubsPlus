2139147859cc51e3c77b52471306782edac710ba
TecktonikMuffin <sot838@gmail.com>
2019-06-12 20:34:31 +1200

    add Clear watchlist cache option

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,8 @@
 setTimeout(function(){
     var cacheDeleteAndRefresh = document.getElementById('delete-cache');
 
+    var WatchListcacheDeleteAndRefresh = document.getElementById('delete-watch-cache');
+
     cacheDeleteAndRefresh.onclick = function(element) {
         // Remove image cache
         chrome.storage.local.set({"imageCache":null}, function(result) {
@@ -10,4 +12,13 @@ setTimeout(function(){
             chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
         });
     };
+    WatchListcacheDeleteAndRefresh.onclick = function(element) {
+        // Remove image cache
+        chrome.storage.local.set({"watchListCache":null}, function(result) {
+        });
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            // Refresh page
+            chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
+        });
+    }
 }, 100);
